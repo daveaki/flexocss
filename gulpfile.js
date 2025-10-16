cd194177cb5d187e6ecd71ed166173c62fc3ce79
daveaki <davide@akilleawhite.it>
2025-10-16 13:46:09 +0200

    rimozione elementi deprecati in sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,44 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-const 
-{
-      series,
-      src,
-      dest,
-      watch,
-      parallel
- 
-} = require('gulp');
-
-const sass      = require('gulp-sass')(require('sass'));
+/* Gulpfile (CommonJS) compatibile con del ESM */
+const { series, src, dest, parallel } = require('gulp');
+const sass    = require('gulp-sass')(require('sass'));
 const concat  = require('gulp-concat');
 const cssmin  = require('gulp-cssmin');
-const rename = require('gulp-rename');
-const del       = require('del');
+const rename  = require('gulp-rename');
 
-const paths  = 
-{
-      'dev_scss': 'public_html/assets/dev/scss/',
-      'dist_css': 'public_html/assets/dist/css/',
+const paths = {
+  dev_scss: 'public_html/assets/dev/scss/',
+  dist_css: 'public_html/assets/dist/css/',
 };
 
-function cleanCssFlexo()
-{
-    return del([paths.dist_css + "test.min.css",]);
-}
-function cssFlexo(done) 
-{
-    src([paths.dev_scss + 'flexo.scss']).pipe(sass()).pipe(concat('flexo.css')).pipe(cssmin()).pipe(rename({suffix: '.min'})).pipe(dest(paths.dist_css));
-    done();
-}
-function cleanCssDevBox()
-{
-    return del([paths.dist_css + "test.min.css",]);
-}
-function cssDevBox(done) 
-{
-     src([paths.dev_scss + 'devbox.scss']).pipe(sass()).pipe(concat('devbox.css')).pipe(rename({suffix: '.min'})).pipe(dest(paths.dist_css));
-    done();
+/** Cleaners (usano import dinamico di del) */
+async function cleanCssFlexo() {
+  const { deleteAsync } = await import('del');
+  return deleteAsync([ paths.dist_css + 'flexo.min.css' ]);
 }
 
+async function cleanCssDevBox() {
+  const { deleteAsync } = await import('del');
+  return deleteAsync([ paths.dist_css + 'devbox.min.css' ]);
+}
+
+/** Build CSS */
+function cssFlexo() {
+  return src([ paths.dev_scss + 'flexo.scss' ])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('flexo.css'))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest(paths.dist_css));
+}
+
+function cssDevBox() {
+  return src([ paths.dev_scss + 'devbox.scss' ])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('devbox.css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest(paths.dist_css));
+}
+
+/** Exports */
 exports.cssFlexo  = series(cleanCssFlexo, parallel(cssFlexo));
-exports.cssDevbox  = series(cleanCssDevBox, parallel(cssDevBox));
+exports.cssDevbox = series(cleanCssDevBox, parallel(cssDevBox));
